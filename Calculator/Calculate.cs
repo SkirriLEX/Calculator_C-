011279451db6ef934bfca6bf8758b450a6ddeede
SkirriLEX <kirk.andri@gmail.com>
2018-03-23 21:46:41 +0200

    Brackets parser

---
+++ Calculator/Calculate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculate
+    {
+        private static char[] action = { '*', '/', '+', '-' };
+
+        public static string compute(string equation_line)
+        {
+            int counter, startCounter;
+            counter = startCounter = 0;
+
+
+            equation_line = filter(equation_line);
+
+            for (int i = 0; i < equation_line.Length; i++)
+            {
+                if (equation_line[i].Equals('('))
+                {
+                    if (counter == 0)
+                    {
+                        startCounter = i;
+                    }
+                    counter++;
+                }
+                if (equation_line[i].Equals(')'))
+                {
+                    counter--;
+                    if (counter == 0)
+                    {
+                        string line = equation_line.Substring(startCounter + 1, i - startCounter - 1);
+                        equation_line = equation_line.Replace("(" + line + ")", Calculate.compute(line));
+                    }
+
+                }
+            }
+            string[] parseNumber = equation_line.Split(new char[] { '+', '-', '/', '*', '(', ')', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] parseAction = equation_line.Split(parseNumber, StringSplitOptions.RemoveEmptyEntries);
+
+            if (parseNumber.Length > 1)
+            {
+                for (int ch = 0; ch < action.Length; ch++)
+                {
+                    string line = parseNumber[0] + action[ch] + parseNumber[0 + 1];
+                    equation_line = equation_line.Replace(line, comparare(parseNumber[0], parseNumber[0], action[ch]));
+                }
+                equation_line = compute(equation_line);
+            }            
+            return equation_line;
+        }
+
+        private static string filter(string equation_line)
+        {
+            char[] filterSymbol = { '+', '-', '/', '*', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '(', ')' };
+            string[] filtered_line = equation_line.Split(filterSymbol, StringSplitOptions.RemoveEmptyEntries);
+
+            if (filtered_line.Length > 0)
+            {
+                equation_line = equation_line.Replace(filtered_line[0], string.Empty);
+                equation_line = filter(equation_line);
+            }
+            return equation_line;
+        }
+
+        private static string comparare(string num_1, string num_2, char action)
+        {
+            string answer = "";
+
+            return answer;
+        }
+
+        private static string calc(string line)
+        {
+            return "xleb";
+        }
+    }
+}
--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,31 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private char[] action = { '*', '/', '+', '-' };
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Symbol_click(object sender, RoutedEventArgs e)
+        {
+            string symbol = (sender as Button).Content.ToString();
+            switch (symbol)
+            {
+                case "±":
+                    break;
+                case "⟵":
+                    Input_line.Text = Input_line.Text.Remove(Input_line.Text.Length - 1, 1);
+                    break;
+                default:
+                    Input_line.Text += (sender as Button).Content;
+                    break;
+            }                
+        }
+        private void Input_line_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Answer_line.Text = Calculate.compute(Input_line.Text);
         }
     }
 }
